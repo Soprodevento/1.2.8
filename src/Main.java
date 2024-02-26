33e23e05a14c2273d8db3a8c5c1982b18ae91ceb
Soprodevento <Sashinka2803@gmail.com>
2024-02-27 01:02:03 +0300

    исправил

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,20 @@
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Main main = new Main();
-        double itog = main.priceCalculation();
-        System.out.println("Цена товара" + itog );
-    }
-    public double priceCalculation(){
         Scanner scanner = new Scanner(System.in);
         System.out.print("Введите цену: ");
         double price = scanner.nextDouble();
-        System.out.print("Введите кол-во товаров : ");
-        double count = scanner.nextInt();
-        double itog = price * count;
-        return  itog;
+
+        System.out.print("Введите кол-во товаров: ");
+        int count = scanner.nextInt();
+
+        Main main = new Main();
+        double itog = main.priceCalculation(price, count);
+        System.out.println("Цена товара: " + itog);
+    }
+
+    public double priceCalculation(double price, int count) {
+        return price * count;
     }
 }
